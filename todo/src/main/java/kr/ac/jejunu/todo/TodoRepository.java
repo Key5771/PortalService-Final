4f7ea06ba9028698f08ac99db20280f7042fc2ad
key5771 <ksj57715@hanmail.net>
2020-06-24 22:25:28 +0900

    TodoRepository 테스트 코드 작성

--- todo/src/main/java/kr/ac/jejunu/todo/TodoRepository.java
+++ todo/src/main/java/kr/ac/jejunu/todo/TodoRepository.java
@@ -1,9 +1,10 @@
 package kr.ac.jejunu.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
-    public List<Todo> findAll();
 }
--- todo/src/test/java/kr/ac/jejunu/todo/TodoRepositoryTests.java
+++ todo/src/test/java/kr/ac/jejunu/todo/TodoRepositoryTests.java
@@ -1,4 +1,31 @@
 package kr.ac.jejunu.todo;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
+
+@DataJpaTest
 public class TodoRepositoryTests {
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    TodoRepository todoRepository;
+
+    @Test
+    public void findAll() {
+        String text = "Spring 공부하기";
+        Todo todo = Todo.builder().text(text).build();
+        entityManager.persist(todo);
+        List<Todo> todos = todoRepository.findAll();
+        assertThat(todos.get(0).getId(), greaterThan(0));
+        assertThat(todos.get(0).getText(), is(text));
+    }
 }
